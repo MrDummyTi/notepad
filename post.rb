3435a26bcd05f1f4b1556e74189290f9dc0ac755
MrDummyTi <ti_odi@mail.ru>
2022-04-06 07:33:44 +0300

    Version 2.0

--- post.rb
+++ post.rb
@@ -1,21 +1,27 @@
 class Post
 
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+  end
+
   def initialize
     @created_at = Time.now
     @text = nil
   end
 
   def read_from_console
-    #to do
   end
 
   def to_strings
-    #to do
   end
 
   def save
     file = File.new(file_path(), "w:UTF8")
-    for item in strings do
+    for item in to_strings do
       file.puts(item)
     end
 
--- task.rb
+++ task.rb
@@ -1,13 +1,23 @@
+require 'date'
 class Task < Post
   def initialize
     super
-
     @due_date = Time.now
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = STDIN.gets.chomp
+
+    puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ, например 12.05.2003"
+    time_completed = STDIN.gets.chomp
+    @due_date = Data.parse(time_completed)
   end
 
   def to_strings
+    time_string = "Созданно: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    dedline = "Крайний срок: #{@due_date}"
+    return [dedline, @text, time_string]
   end
 end
